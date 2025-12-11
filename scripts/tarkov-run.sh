53ac8cee71868c05afce90e64f2bfb8237d118f0
nmlynch94 <nmlynch94@gmail.com>
2025-12-11 16:05:08 -0500

    latest

--- scripts/tarkov-run.sh
+++ scripts/tarkov-run.sh
@@ -16,24 +16,26 @@ if [[ ! -f "$tarkov_launcher_exe_path" ]]; then
   echo "Tarkov launcher not installed. Installing..."
   # Install dotnet48 and launch
   # TODO mouse focus registry key
-  umu-run winetricks -q dotnet48 vcrun2022
+  # Use umu-latest to install deps because dotnet48 is broken in proton 10+
+  PROTONPATH="UMU-Latest" umu-run winetricks -q dotnet48 vcrun2022
   curl -o "$tarkov_installer_name" -L "$TARKOV_LAUNCHER_URL"
 
   #BE workaround
   if zenity --question \
       --title="Info" \
-      --text="In the next step, install the game. If it says to update after it completes, update. Click update if applicable until no more updates are available, and then close the launcher. Click yes to continue, or click no to abort."; then
+      --text="In the next step, install the game. If it says to update after it completes, update. Click update if applicable until no more updates are available, and then close the launcher. UNCHECK LAUNCH AFTER INSTALL. Click yes to continue, or click no to abort."; then
     echo "Continuing..."
   else
     exit 0
   fi
   umu-run "$tarkov_installer_name"
   rm "$tarkov_installer_name"
-  mkdir -p "$WINEPREFIX/drive_c/Battlestate Games/BsgLauncher"
+  umu-run  "$WINEPREFIX/drive_c/Battlestate Games/BsgLauncher/BsgLauncher.exe" --disable-softare-rasterizer
   mkdir -p "$WINEPREFIX/drive_c/Program Files (x86)/Common Files/BattlEye"
   echo "d3d9.shaderModel = 1" > "$WINEPREFIX/drive_c/Battlestate Games/BsgLauncher/dxvk.conf"
   echo "Launcher closed. Performing BE workaround..."
-  cp "$WINEPREFIX/drive_c/Battlestate Games/Escape from Tarkov/BattlEye/BEService_x64.exe" "$WINEPREFIX/drive_c/Program Files (x86)/Common Files/BattlEye/BEService_x64.exe" || true
+  cp "$WINEPREFIX/drive_c/Battlestate Games/Escape from Tarkov/BattlEye/BEService_x64.exe" "$WINEPREFIX/drive_c/Program Files (x86)/Common Files/BattlEye/BEService_x64.exe"
+
   SERVICE_KEY='HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\BEService'
 
   # REG_SZ
@@ -47,12 +49,26 @@ if [[ ! -f "$tarkov_launcher_exe_path" ]]; then
   umu-run reg add "$SERVICE_KEY" /v Start /t REG_DWORD /d 2 /f
   umu-run reg add "$SERVICE_KEY" /v Type /t REG_DWORD /d 16 /f
   umu-run reg add "$SERVICE_KEY" /v WOW64 /t REG_DWORD /d 1 /f
+
+  umu-run dotnet-runtime.exe /q
+  umu-run aspnet-core.exe /q
   # umu-run reg add "HKEY_CURRENT_USER\Software\Wine\X11 Driver" /v UseTakeFocus /t REG_DWORD /s "N" /f
-  umu-run "$tarkov_launcher_exe_path" --disable-software-rasterizer
+  # SPT Installer run
+  mkdir -p "$WINEPREFIX/drive_c/SPT"
+  PROTONPATH="UMU-Latest" umu-run winetricks -q arial times dotnetdesktop6 dotnetdesktop8 dotnetdesktop9
+  curl -o "dotnet-runtime.exe" -L https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/9.0.11/windowsdesktop-runtime-9.0.11-win-x64.exe
+  curl -o "aspnet-core.exe" -L https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.11/aspnetcore-runtime-9.0.11-win-x64.exe
+  umu-run "SPTInstaller.exe" installpath="C:\SPT"
+  chmod +x "$WINEPREFIX/drive_c/SPT/SPT/SPT.Server.Linux"
+  umu-run winecfg /v win81
+
 
   exit 0
 else
-  umu-run "$tarkov_launcher_exe_path" --disable-software-rasterizer
+
+   cd "$WINEPREFIX/drive_c/SPT/SPT"
+   ./"SPT.Server.Linux" &
+   WINEDLLOVERRIDES="winhttp=n,b" umu-run "SPT.Launcher.exe"
 
   exit 0
 fi
